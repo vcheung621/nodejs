cccfccbffb4c96da0ff80931210940e0a4670586
Vincent Cheung <vcheung621@hotmail.com>
2020-06-25 23:21:54 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const person = require('./person');
+
+console.log(person);
+console.log(person.name)
---
+++ index2.js
@@ -0,0 +1,5 @@
+const Person = require('./person2');
+
+const person1 = new Person('John Doe', 30);
+
+person1.greeting();
