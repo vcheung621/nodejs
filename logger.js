98629e529a049ca80b1ecb1503da78d261343faf
Vincent Cheung <vcheung621@hotmail.com>
2020-06-27 18:01:16 -0700

    first commit

---
+++ logger.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+const uuid = require('uuid');
+
+console.log(uuid.v4());
+console.log(uuid.v4());
+console.log(uuid.v4());
+console.log(uuid.v4());
+
+class Logger extends EventEmitter {
+	log(msg) {
+		// Call event
+		this.emit('message', { id: uuid.v4(), msg });
+	}
+}
+
+module.exports = Logger;
